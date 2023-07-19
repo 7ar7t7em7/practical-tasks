ea9d39e243c41593a506f6409ef601843babda54
Artem_Dubinskiy <ar7t7em7@yandex.ru>
2023-07-19 13:22:30 +0300

    myHomework_lesson1_task1

---
+++ src/com/walking/lesson1_variablesMyHomewor/MyHomework.java
@@ -0,0 +1,13 @@
+package com.walking.lesson1_variablesMyHomewor;
+
+public class MyHomework {
+
+	public static void main(String[] args) {
+		int a, b;
+		a=1;
+		b=2;
+		System.out.println(a + " + " + b);
+
+	}
+
+}
