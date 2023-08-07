4ae1524c6560adbda8ad61861b73727bae181455
Artem_Dubinskiy <ar7t7em7@yandex.ru>
2023-08-07 18:35:37 +0300

    урок 19 машины

--- src/com/walking/HomeWork_lesson19_Object/Car.java
+++ src/com/walking/HomeWork_lesson19_Object/Car.java
@@ -6,10 +6,16 @@ public class Car {
 	private final 	String 	brand;
 	private final 	String	model;
 	private final 	String	VIN;
-	private 		int 	number;
+	private 		String 	number;
 	
+	Car(){
+		this.brand = "";
+		this.model = "";
+		this.number = "";
+		this.VIN = "";
+	}
 	
-	Car(String brand, String model, int number, String vin){
+	Car(String brand, String model, String number, String vin){
 		this.brand = brand;
 		this.model = model;
 		this.VIN = vin;
@@ -35,6 +41,47 @@ public class Car {
 		return Objects.equals(VIN, other.VIN) && Objects.equals(brand, other.brand)
 				&& Objects.equals(model, other.model) && number == other.number;
 	}
+	@Override
+	public String toString() {
+		return "Brand: " + this.brand + "\nModel: " + this.model + "\nNumber: " + this.number + "\nVIN: " + this.VIN + "\n";
+	}
 	
+	public static Car[] findCar(Car[] carsForSearch, String str, int field) {
+		Car[] carsForReturn = new Car[carsForSearch.length];
+		if (field == 1) {
+			int x = 0;
+			for (Car car : carsForSearch) {
+				if(car.brand.equalsIgnoreCase(str)) {
+					carsForReturn[x] = car;
+					x++;
+				}
+			}
+		}else if(field == 2) {
+			int x = 0;
+			for (Car car : carsForSearch) {
+				if(car.model.equalsIgnoreCase(str)) {
+					carsForReturn[x] = car;
+					x++;
+				}
+			}
+		}else if(field == 3) {
+			int x = 0;
+			for (Car car : carsForSearch) {
+				if(car.number.equalsIgnoreCase(str)) {
+					carsForReturn[x] = car;
+					x++;
+				}
+			}
+		}else if(field == 4) {
+			int x = 0;
+			for (Car car : carsForSearch) {
+				if(car.VIN.equalsIgnoreCase(str)) {
+					carsForReturn[x] = car;
+					x++;
+				}
+			}
+		}
+		return carsForReturn;
+	}
 	
 }
--- src/com/walking/HomeWork_lesson19_Object/Main.java
+++ src/com/walking/HomeWork_lesson19_Object/Main.java
@@ -1,5 +1,50 @@
 package com.walking.HomeWork_lesson19_Object;
 
-public class Main {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		Car[] cars = {
+						new Car("Daewoo", "Lanos", "V123RK", "123ZZZ"),
+						new Car("Daewoo", "Sens", "V222RK", "123XXX"),
+						new Car("Toyota", "Rav4", "Z456Rv", "qweXXX"),
+						new Car("Mercedes", "G450", "V422RK", "asdXXX"),
+						new Car("Lada", "Granta", "V444RK", "1zxcXXX"),
+						new Car("Chevrolet", "Aveo", "V334RK", "123Xyu")};
+		Car[] findCars = new Car[cars.length];
+		String str = "";
+		do {
+			System.out.println("Search by:\n");
+			System.out.println("1 - brand, 2 - model, 3 - number, 4 - VIN");
+			int searchBy = sc.nextInt();
+			if (searchBy != 1 && searchBy != 2 && searchBy != 3 && searchBy != 4) {
+				System.out.println("Incorrect!\n Try again");
+				System.exit(0);
+			}
+			if(searchBy == 1) {
+				System.out.println("Enter brand:\n");
+				str = sc.next();
+			}else if(searchBy == 2) {
+				System.out.println("Enter model:\n");
+				str = sc.next();
+			}else if(searchBy == 3) {
+				System.out.println("Enter number:\n");
+				str = sc.next();
+			}else if(searchBy == 4) {
+				System.out.println("Enter VIN:\n");
+				str = sc.next();
+			}
+			findCars = Car.findCar(cars, str, searchBy);
+		} while ((findCars[0] != null && findCars[1] == null) && findCars[0] == null);
+		for (Car car : findCars) {
+			if(car == null) {
+				continue;
+			}
+			System.out.println(car.toString());
+		}
+	}
+	
 }
